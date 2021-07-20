2259d02f6da61a1ba0bbc7d9c72fcab8c80eb525
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-19 21:20:15 -0700

    Updating image names again.

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
 // const bootstrap = require("bootstrap");
 
 
-{/* <div class="col-md-4">
-                        <a href="https://jojonah1812.github.io/E-Commerce-Back-End-ORM/" target="_blank"><img
-                        src="./assets/images/project-images/ecommerceCOMMERCIAL.jpg" alt="eCommerce project"
-                        class="float-left"></a>
-                    </div> */}
